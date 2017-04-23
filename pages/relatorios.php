c3c4461aeecede51d1c9b95c8decb4cd09cd321a
Robson Flores <robsonflowers@gmail.com>
2017-04-23 16:21:59 -0300

    Atualizada pagina de relatorios

--- pages/relatorios.php
+++ pages/relatorios.php
@@ -48,24 +48,23 @@ include_once(BASE_PATH.'app/classes/Mini.dao.php');
 </div>  
 
 <?php $somaTotal = MiniDAO::totalDescPorTecnico();?>
-
-
 <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
     <div class="row">
         <div class="panel panel-default">
+            <div class="panel-heading">Total de mini isolators - por técnico</div>
             <div class="panel-body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped table-condensed text-center" >
                         <thead>
                             <tr>
-                                <th class="">Nome</th>
-                                <th class="">Total</th>
+                                <th class="text-center">Colaborador</th>
+                                <th class="text-center">Total (inst+subst)</th>
                             </tr>
                         </thead>
                         <tbody>
                         <?php foreach($somaTotal as $somaTotal):?>
                         <tr>
-                            <td>
+                            <td class="text-left">
                             <?php echo $somaTotal['colaborador_instalou'];?>
                             </td>
                             <td>
@@ -79,5 +78,37 @@ include_once(BASE_PATH.'app/classes/Mini.dao.php');
             </div>    
         </div>
     </div>
+</div>
 
+<?php $somaTotalNode = MiniDAO::totalDescPorNode();?>
+<div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
+    <div class="row">
+        <div class="panel panel-default">
+            <div class="panel-heading">Total de mini isolators - por Node</div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-condensed text-center" >
+                        <thead>
+                            <tr>
+                                <th class="text-center">Node</th>
+                                <th class="text-center">Total (inst+subst)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($somaTotalNode as $somaTotalNode):?>
+                        <tr>
+                            <td>
+                            <?php echo $somaTotalNode['node'];?>
+                            </td>
+                            <td>
+                            <?php echo $somaTotalNode['total'];?>    
+                            </td>
+                        </tr>
+                        <?php endforeach;?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>    
+        </div>
+    </div>
 </div>    
